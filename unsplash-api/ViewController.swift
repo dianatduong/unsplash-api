5655f6b0359250f65d90d1443bee7b6193e8b1ca
“Diana <dduong013@gmail.com>
2023-07-04 13:39:02 -0500

    add postman api request

--- unsplash-api/ViewController.swift
+++ unsplash-api/ViewController.swift
@@ -12,9 +12,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemGray
+        
+        fetchAPI()
     }
 
 
+    
+    
+    func fetchAPI() {
+        var request = URLRequest(url: URL(string: "https://api.unsplash.com/search/photos?client_id=SwsdyqI6m6t38pMRrT8uCyXd-6-AKdT5Dy8I76IpEtc&count=1&query=yellowstone&page=1&per_page=1&orientation=landscape&order_by=latest")!,timeoutInterval: Double.infinity)
+        request.httpMethod = "GET"
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+          guard let data = data else {
+            print(String(describing: error))
+            return
+          }
+          print(String(data: data, encoding: .utf8)!)
+        }
+
+        task.resume()
+    }
+    
+    
+    
 }
 
